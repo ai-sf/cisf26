2d0c26458910f69740c0bab1e8120ce8c2af0057
Orion551 <gravanteumberto@outlook.com>
2025-11-17 16:28:36 +0100

    + cisf26 info text

--- themes/cisf26-theme/assets/js/subscriptions.js
+++ themes/cisf26-theme/assets/js/subscriptions.js
@@ -32,46 +32,7 @@ export function initSubscriptionsPage() {
     const stepTexts = {
         1: {
             title: "Cominciamo",
-            subtitle: `
-            Benvenut nel modulo di registrazione alla X Conferenza Italiana Studenti di Fisica, che si terrà dal <strong>13 al 17 Aprile 2026</strong> a <strong>Roma</strong>.<br><br>
-
-            Tutte le informazioni sull'evento sono disponibili sul sito di CISF26. Per contattare il Comitato Organizzatore puoi scrivere a cisf26@ai-sf.it o utilizzare il modulo presente in home page.<br><br>
-
-            Puoi modificare le tue risposte dopo aver inviato il form, se strettamente necessario.<br><br>
-
-            <strong>1. Informazioni Generali:</strong><br>
-            Le iscrizioni chiuderanno il <strong>18 Dicembre 2025</strong> alle 23.59.<br><br>
-
-            La selezione dei/delle partecipanti sarà effettuata dal Comitato Organizzatore della CISF26 in collaborazione con il Comitato Esecutivo di AISF. La lettera di motivazione, da compilare nel presente modulo di registrazione, sarà presa in considerazione come importante elemento di valutazione.<br><br>
-
-            Entro il <strong>10 Gennaio 2026</strong> i/le partecipanti selezionati/e saranno avvisati/e tramite l'indirizzo email indicato in fase di registrazione. Si prega a tal proposito di controllare quotidianamente la casella di posta.<br><br>
-
-            Sarà quindi richiesto il pagamento della quota di iscrizione entro e non oltre Giovedì <strong>15 Gennaio 2026</strong> alle ore 23:59, pena l’esclusione della partecipazione all’evento.<br><br>
-
-            Ti informiamo che la quota di partecipazione non è in nessun caso rimborsabile.<br><br>
-
-            Una volta definiti i/le partecipanti/e all'evento verrà pianificato il calendario delle Parallel Sessions. Non è garantito che tutti i contributi scientifici potranno essere accettati.<br><br>
-
-            <strong>2. Quota di Partecipazione</strong><br>
-
-            Sono disponibili in totale 150 posti. Le quote di partecipazione per l'evento sono:<br>
-            - 120 euro (con alloggio);<br>
-            - 70 euro (senza alloggio).<br><br>
-
-            Entrambe le quote di partecipazione comprendono:<br>
-            - Una colazione, un pranzo, il pranzo sociale, la serata finale e i coffee break;<br>
-            - Welcome kit;<br>
-            - Soggiorno presso la casa per ferie Seraphicum per le 4 notti dell'evento (solo quota con alloggio).<br><br>
-
-            Entrambe le quote di partecipazione non comprendono:<br>
-            - Spese di viaggio per raggiungere la conferenza;<br>
-            - Assicurazione.<br><br>
-
-            Per chi non è ancora membro AISF alla quota di registrazione all'evento andrà aggiunta, al costo di 5 euro (10 euro per gli studenti di dottorato, 20 per gli esterni), la quota di iscrizione all'Associazione per l'anno 2025/2026. <br>
-            Per informazioni consultare https://ai-sf.it/iscrizione/. <br>
-            La quota di iscrizione all'AISF include la membership IAPS (International Association of Physics Students) di cui AISF è National Committee per l'italia.<br><br>
-
-            Per motivi amministrativi, ti chiediamo Nome e Cognome che devono essere quelli presenti sulla carta d’identità.
+            subtitle: `Per motivi amministrativi, ti chiediamo Nome e Cognome che devono essere quelli presenti sulla carta d’identità.
             Se vuoi, puoi indicare anche un nome alias, che useremo su badge e comunicazioni ufficiali.
         `
         },
@@ -370,4 +331,32 @@ export function initSubscriptionsPage() {
 
     updateProgress();
     updateStepTexts(1);
+
+    document.addEventListener('click',  async (ev) => {
+        const trigger = ev.target.closest('[data-modal-target]');
+        if (trigger) {
+            ev.preventDefault();
+            const modalId = trigger.dataset.modalTarget || '';
+            const selector = modalId.startsWith('#') ? modalId : `#${modalId}`;
+            const modal = document.querySelector(selector);
+            openModal(modal, trigger);
+            return;
+        }
+    })
+
+    const openModal =  async (modal, trigger) => {
+        if(!modal) return;
+        if (modal && trigger) {
+            const response = await fetch('/cisf26/modal-content/cisf-subscription-info.md');
+            const markdown = await response.text();
+
+            modal.querySelector('.modal-info.body').innerHTML = marked.parse(markdown);
+
+            modal.classList.add('open');
+            document.body.classList.add('modal-open');
+
+            const pageContent = document.querySelector('.page-content');
+            pageContent.classList.add('page-content-modal-open');
+        }
+    }
 }
